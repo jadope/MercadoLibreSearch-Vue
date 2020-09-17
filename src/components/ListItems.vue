73735aba83fe53470924be6e687615d8d42753e0
= <=>
2020-09-17 16:09:02 -0500

    Mejorando guía de estilo

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
     <div class="list-group">
         <Items 
         :key="item.id" 
@@ -9,12 +8,6 @@
         :imgUrl="item.thumbnail" 
         :seller="item.seller.id">
         </Items>
-=======
-    <div  class="list-group">
-        <ul class="list-group-item">
-            <li><Items :key="obj" v-for="obj in object" :title="obj.title" :price="obj.price" :imgUrl="obj.thumbnail"></Items> </li>        
-        </ul>
->>>>>>> b0376dc41af5a6087358bdaccd4c3534c16fab00
     </div>
 </template>
 
@@ -22,11 +15,7 @@
 <script>
 import Items from './Item.vue'
 export default {
-<<<<<<< HEAD
 name: 'ListItems',
-=======
-name: 'ItemsList',
->>>>>>> b0376dc41af5a6087358bdaccd4c3534c16fab00
 props: {
     object: []
   },
@@ -37,13 +26,4 @@ props: {
 </script>
 
 <style>
-<<<<<<< HEAD
-
-=======
-    .centralizar{
-        display: block;
-    margin-left: auto;
-    margin-right: auto
-    }
->>>>>>> b0376dc41af5a6087358bdaccd4c3534c16fab00
 </style>
